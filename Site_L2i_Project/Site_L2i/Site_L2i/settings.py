4a00db914fd5c83c2895127907b4e6f0c662289f
AlHamine <103815257+AlHamine@users.noreply.github.com>
2022-08-30 06:59:50 +0000

    Update settings.py

--- Site_L2i_Project/Site_L2i/Site_L2i/settings.py
+++ Site_L2i_Project/Site_L2i/Site_L2i/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'face'
+    'face',
 ]
 
 MIDDLEWARE = [
@@ -122,8 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     ]
-
+MEDIA_ROOT='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
