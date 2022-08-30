e09adb65e0f9607cb71c02866bdeab4f0bc5fc1e
gaye-00 <110274977+gaye-00@users.noreply.github.com>
2022-08-30 02:17:02 +0200

    Ajout du template

--- Site_L2i_Project/Site_L2i/Site_L2i/urls.py
+++ Site_L2i_Project/Site_L2i/Site_L2i/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import html
 from msilib.schema import LockPermissions
 from django.contrib import admin
 from django.urls import path, include
+from face import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('face/', include('face.urls'))
+    path('face', include('face.urls')),
+    path('', views.index),
 ]
