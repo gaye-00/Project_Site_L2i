e59c80507deeae77d63b66c0c10400c91d72e50f
AlHamine <103815257+AlHamine@users.noreply.github.com>
2022-09-13 23:29:22 +0000

    Delete Site_L2i_Project/Site_L2i directory

--- Site_L2i_Project/Site_L2i/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.logIn , name='login'),
-    path('logout', views.logOut , name='logout'),
-    path('register', views.register, name='register'),
-]
