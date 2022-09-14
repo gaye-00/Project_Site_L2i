eab16cc5678e9baf68d303a8ce5d2c963d7a9b3a
gaye-00 <110274977+gaye-00@users.noreply.github.com>
2022-09-14 03:07:50 +0200

    ajout4

--- Site_L2i_Project/Site_L2i/Site_L2i/urls.py
+++ Site_L2i_Project/Site_L2i/Site_L2i/urls.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 """Site_L2i URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -25,6 +26,34 @@ urlpatterns = [
     path('face', include('face.urls')),
     path('', views.index),
 ]
+=======
+"""Site_L2i URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from cgitb import html
+from msilib.schema import LockPermissions
+from django.contrib import admin
+from django.urls import path, include
+from face import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('face', include('face.urls')),
+    path('', views.index),
+]
+>>>>>>> parent of c8dc60d (Authentification)
 =======
 """Site_L2i URL Configuration
 
